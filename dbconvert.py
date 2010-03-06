adb73e3f1791c109b3d9754939f1ac07cb74d9fa
Michael Elsdoerfer <michael@elsdoerfer.com>
2010-03-06 01:51:59 +0100

    Added an -all option to process all tables from the source server.

--- dbconvert.py
+++ dbconvert.py
@@ -2,7 +2,7 @@
 """
 Script to convert between different databases.
 
-Written against SQLAlchemy 5.3; tested with 6.1beta.
+Written against SQLAlchemy 6.1beta.
 
 Based on code from:
 http://www.tylerlesmann.com/2009/apr/27/copying-databases-across-platforms-sqlalchemy/
@@ -14,20 +14,23 @@ from sqlalchemy import create_engine, MetaData, Table
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+
 def make_session(connection_string):
     engine = create_engine(connection_string, echo=False, convert_unicode=True)
     Session = sessionmaker(bind=engine)
     return Session(), engine
+
 
 def pull_data(from_db, to_db, tables):
     source, sengine = make_session(from_db)
     smeta = MetaData(bind=sengine)
     destination, dengine = make_session(to_db)
 
-    for table_name in tables:
-        print 'Processing', table_name
-        print 'Pulling schema from source server'
-        table = Table(table_name, smeta, autoload=True)
+    print 'Pulling schemas from source server'
+    smeta.reflect(only=tables or None)
+
+    for name, table in smeta.tables.iteritems():
+        print 'Processing', name
         print 'Creating table on destination server'
         table.metadata.create_all(dengine)
         NewRecord = quick_mapper(table)
@@ -41,14 +44,16 @@ def pull_data(from_db, to_db, tables):
     print 'Committing changes'
     destination.commit()
 
+
 def print_usage():
     print """
-Usage: %s -f source_server -t destination_server table [table ...]
+Usage: %s -f source_server -t destination_server table [--all] [table ...]
     -f, -t = driver://user[:password]@host[:port]/database
 
 Example: %s -f oracle://someuser:PaSsWd@db1/TSH1 \\
     -t mysql://root@db2:3307/reporting table_one table_two
     """ % (sys.argv[0], sys.argv[0])
+
 
 def quick_mapper(table):
     Base = declarative_base()
@@ -56,16 +61,22 @@ def quick_mapper(table):
         __table__ = table
     return GenericMapper
 
-if __name__ == '__main__':
-    optlist, tables = getopt.getopt(sys.argv[1:], 'f:t:')
+
+def main():
+    optlist, tables = getopt.getopt(sys.argv[1:], 'f:t:', ['all'])
 
     options = dict(optlist)
-    if '-f' not in options or '-t' not in options or not tables:
+    if '-f' not in options or '-t' not in options or not (
+        bool(tables) ^ bool('--all' in options)):
         print_usage()
-        raise SystemExit, 1
+        return 1
 
     pull_data(
         options['-f'],
         options['-t'],
         tables,
     )
+
+
+if __name__ == '__main__':
+    sys.exit(main() or 0)
