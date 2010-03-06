52bb54b7cee161d33c1894679264dd65a48b0ece
Michael Elsdoerfer <michael@elsdoerfer.com>
2010-03-06 02:09:14 +0100

    Added an option to skip the schema generation on the target database.

--- dbconvert.py
+++ dbconvert.py
@@ -21,7 +21,7 @@ def make_session(connection_string):
     return Session(), engine
 
 
-def pull_data(from_db, to_db, tables):
+def pull_data(from_db, to_db, tables, create_schema):
     source, sengine = make_session(from_db)
     smeta = MetaData(bind=sengine)
     destination, dengine = make_session(to_db)
@@ -31,8 +31,9 @@ def pull_data(from_db, to_db, tables):
 
     for name, table in smeta.tables.iteritems():
         print 'Processing', name
-        print 'Creating table on destination server'
-        table.metadata.create_all(dengine)
+        if create_schema:
+            print 'Creating table on destination server'
+            table.metadata.create_all(dengine)
         NewRecord = quick_mapper(table)
         columns = table.columns.keys()
         print 'Transferring records'
@@ -47,7 +48,7 @@ def pull_data(from_db, to_db, tables):
 
 def print_usage():
     print """
-Usage: %s -f source_server -t destination_server table [--all] [table ...]
+Usage: %s -f source_server -t destination_server table [--all] [--skip-schema] [table ...]
     -f, -t = driver://user[:password]@host[:port]/database
 
 Example: %s -f oracle://someuser:PaSsWd@db1/TSH1 \\
@@ -63,7 +64,7 @@ def quick_mapper(table):
 
 
 def main():
-    optlist, tables = getopt.getopt(sys.argv[1:], 'f:t:', ['all'])
+    optlist, tables = getopt.getopt(sys.argv[1:], 'f:t:', ['all', 'skip-schema'])
 
     options = dict(optlist)
     if '-f' not in options or '-t' not in options or not (
@@ -75,6 +76,7 @@ def main():
         options['-f'],
         options['-t'],
         tables,
+        not '--skip-schema' in options
     )
 
 
